ee4fea69f5897aba9d8e4c22a3bbcf152f08dfe4
yandongxu0821 <yandongxu0821@qq.com>
2024-08-27 17:02:02 +0800

    feat: 新增对 QQ 部分广告关闭规则
    修改现有com.tencent.mobileqq.ts, 添加了部分规则, 同时删除了关于 微信 和 GKD 的
    规则部分，预期不会进行关于此的适配。

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -3,5 +3,62 @@ import { defineGkdApp } from '@gkd-kit/define';
 export default defineGkdApp({
   id: 'com.tencent.mobileqq',
   name: 'QQ',
-  groups: [],
+  groups: [
+    {
+      name: '右上角推广1',
+      key: 0,
+      desc: '关闭',
+      rules: [
+        {
+          position: {
+            top: 'height*0.2626',
+            right: 'width*0.1094',
+          },
+          matches: ['[id="com.tencent.mobileqq:id/yxk"]'],
+          fastQuery: true,
+          activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+        },
+      ],
+    },
+    {
+      name: '开屏广告1',
+      key: 1,
+      desc: '跳过广告',
+      rules: [
+        {
+          matches: ['[id="com.tencent.mobileqq:id/dzp"]'],
+          fastQuery: true,
+          activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+        },
+      ],
+    },
+    {
+      name: '搜索栏上方横幅广告',
+      key: 2,
+      desc: '关闭',
+      rules: [
+        {
+          matches: ['[id="com.tencent.mobileqq:id/dij"]'],
+          fastQuery: true,
+          activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+        },
+      ],
+    },
+    {
+      name: '右上角推广0',
+      key: 3,
+      desc: '关闭',
+      rules: [
+        {
+          position: {
+            top: 'height*0.2626',
+            right: 'width*0.1094',
+          },
+          matches: ['[id="com.tencent.mobileqq:id/z2y"]'],
+          fastQuery: true,
+          activityIds: ['com.tencent.mobileqq.activity.SplashActivity'],
+        },
+      ],
+    },
+  ],
 });
